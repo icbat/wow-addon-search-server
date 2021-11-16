8ebda9ec203e2d21155b99c49672d8502c4a1e45
EvanCobb <evan.cobb@protonmail.com>
2021-11-15 22:38:33 -0600

    adds very basic caching
    just so we don't blow out the API token before setting things up

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import Koa from 'koa'
 import { getPage } from './github.js'
+import cache from './cache.js'
 
 const app = new Koa();
 const githubApiToken = process.env.GITHUB_API_TOKEN
 
 app.use(async ctx => {
+    const cached = cache.get()
+    if (cached) {
+        return ctx.body = cached
+    }
+
     const page = await getPage(githubApiToken)
+
+    cache.set(cached)
     ctx.body = page
 });
 
