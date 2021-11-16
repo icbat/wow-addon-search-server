1def25a3ad81ca794958f5e3256ce8aa1ab2da31
EvanCobb <evan.cobb@protonmail.com>
2021-11-15 22:42:40 -0600

    adds missing file
    stupid add -am :)

---
+++ cache.js
@@ -0,0 +1,30 @@
+const lastTime = null
+const lastResponse = null
+
+const cacheExpirationMs = process.env.CACHE_EXPIRATION_MS || 5 * 60 * 1000
+
+export const get = () => {
+    const now = Date.now()
+
+    if (!lastTime || !lastResponse) {
+        console.log('No cache yet')
+        return null
+    }
+
+    if (now > lastTime + cacheExpirationMs) {
+        console.log('Expired after', cacheExpirationMs, 'ms')
+        console.log('resetting cache')
+        lastTime = null
+        return null
+    }
+
+    console.log('returning cached response')
+    return lastResponse
+}
+
+export const set = (newVal) => {
+    lastResponse = newVal
+    lastTime = Date.now()
+}
+
+
